c3b32a7e898ca85a98137d71ad8c21e72b615804
Son Do <dominhson580@gmail.com>
2023-05-22 21:45:12 +0700

    Draw the maze to the screen

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from graphic import Window
+from graphics import Window, Maze
+
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 700
+
+LEFT_EDGE = 50
+TOP_EDGE = 50
+
+CELL_SIZE = 50
+
+num_rows = (WINDOW_HEIGHT - 2 * TOP_EDGE) // CELL_SIZE
+num_cols = (WINDOW_WIDTH - 2 * LEFT_EDGE) // CELL_SIZE
+
 
 def main():
-    window = Window(800, 600)
+    window = Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    Maze(LEFT_EDGE, TOP_EDGE, num_rows, num_cols, CELL_SIZE, window)
+
     window.wait_for_close()
 
-main() 
+
+main()
