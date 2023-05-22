3df089d0e079a9c8e8405b62167004cb35812b8b
Son Do <dominhson580@gmail.com>
2023-05-22 22:07:41 +0700

    Draw background color for non-existing wall

--- graphics.py
+++ graphics.py
@@ -73,18 +73,33 @@ class Cell:
         self.__window = window
 
     def draw(self):
+        top_left = Point(self.__x1, self.__y1)
+        bottom_left = Point(self.__x1, self.__y2)
+        top_right = Point(self.__x2, self.__y1)
+        bottom_right = Point(self.__x2, self.__y2)
+
         if self.has_left_wall:
-            self.__window.draw_line(
-                Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)), WALL_COLOR)
+            self.__window.draw_line(Line(top_left, bottom_left), WALL_COLOR)
+        else:
+            self.__window.draw_line(Line(top_left, bottom_left), BG_COLOR)
+
         if self.has_right_wall:
+            self.__window.draw_line(Line(top_right, bottom_right), WALL_COLOR)
+        else:
             self.__window.draw_line(
-                Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)), WALL_COLOR)
+                Line(top_right, bottom_right), BG_COLOR)
+
         if self.has_top_wall:
-            self.__window.draw_line(
-                Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)), WALL_COLOR)
+            self.__window.draw_line(Line(top_left, top_right), WALL_COLOR)
+        else:
+            self.__window.draw_line(Line(top_left, top_right), BG_COLOR)
+
         if self.has_bottom_wall:
             self.__window.draw_line(
-                Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)), WALL_COLOR)
+                Line(bottom_left, bottom_right), WALL_COLOR)
+        else:
+            self.__window.draw_line(
+                Line(bottom_left, bottom_right), BG_COLOR)
 
     def get_center(self):
         return Point((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2)
