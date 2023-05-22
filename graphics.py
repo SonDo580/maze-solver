4d058a272b1f8a0a5520350d0bd476c1b40352e0
Son Do <dominhson580@gmail.com>
2023-05-22 17:14:52 +0700

    Create method for moving from 1 cell to another

--- graphics.py
+++ graphics.py
@@ -1,7 +1,9 @@
 from tkinter import Tk, BOTH, Canvas
 
 BG_COLOR = 'white'
-LINE_COLOR = 'black'
+WALL_COLOR = 'black'
+PATH_COLOR = 'red'
+UNDO_PATH_COLOR = 'gray'
 
 LINE_WIDTH = 2
 
@@ -29,8 +31,8 @@ class Window:
     def close(self):
         self.__running = False
 
-    def draw_line(self, line):
-        line.draw(self.__canvas)
+    def draw_line(self, line, color):
+        line.draw(self.__canvas, color)
 
 
 # x = 0: left of window
@@ -47,8 +49,8 @@ class Line:
         self.__start = start
         self.__end = end 
     
-    def draw(self, canvas):
-        canvas.create_line(self.__start.x, self.__start.y, self.__end.x, self.__end.y, fill=LINE_COLOR, width=LINE_WIDTH)
+    def draw(self, canvas, color):
+        canvas.create_line(self.__start.x, self.__start.y, self.__end.x, self.__end.y, fill=color, width=LINE_WIDTH)
         canvas.pack(fill=BOTH, expand=True)
 
 # x1, y1: top-left
@@ -67,11 +69,21 @@ class Cell:
 
     def draw(self):
         if self.has_left_wall:
-            self.__window.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)))
+            self.__window.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)), WALL_COLOR)
         if self.has_right_wall:
-            self.__window.draw_line(Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)))
+            self.__window.draw_line(Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)), WALL_COLOR)
         if self.has_top_wall:
-            self.__window.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)))
+            self.__window.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)), WALL_COLOR)
         if self.has_bottom_wall:
-            self.__window.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)))
+            self.__window.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)), WALL_COLOR)
+    
+    def get_center(self):
+        return Point((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2)
+    
+    def draw_move(self, to_cell, undo=False):
+        color = PATH_COLOR
+        if undo:
+            color = UNDO_PATH_COLOR
+
+        self.__window.draw_line(Line(self.get_center(), to_cell.get_center()), color)
         
