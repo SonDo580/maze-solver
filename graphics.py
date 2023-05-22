d5d0a2ea79479ed361154a3af8a7bdc942b4d293
Son Do <dominhson580@gmail.com>
2023-05-22 16:55:36 +0700

    Create class Cell

--- graphics.py
+++ graphics.py
@@ -2,6 +2,7 @@ from tkinter import Tk, BOTH, Canvas
 
 BG_COLOR = 'white'
 LINE_COLOR = 'black'
+
 LINE_WIDTH = 2
 
 class Window:
@@ -49,4 +50,28 @@ class Line:
     def draw(self, canvas):
         canvas.create_line(self.__start.x, self.__start.y, self.__end.x, self.__end.y, fill=LINE_COLOR, width=LINE_WIDTH)
         canvas.pack(fill=BOTH, expand=True)
+
+# x1, y1: top-left
+# x2, y2: bottom-right
+class Cell:
+    def __init__(self, x1, y1, x2, y2, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        self.__window = window
+
+    def draw(self):
+        if self.has_left_wall:
+            self.__window.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)))
+        if self.has_right_wall:
+            self.__window.draw_line(Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)))
+        if self.has_top_wall:
+            self.__window.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)))
+        if self.has_bottom_wall:
+            self.__window.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)))
         
